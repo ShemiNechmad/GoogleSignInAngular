8c71ed29b7569b13b1b9b95d2d87f26d2367ebbc
Shemi Nechmad <crunchies@mail.com>
2022-07-11 09:48:27 +0300

    callback syntax changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,22 +14,24 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     google.accounts.id.initialize({
       client_id: "236025958894-l05tha7iovc0ool81upch4i6gi91npe8.apps.googleusercontent.com",
-      callback: (response: any) => {
-        console.log(response.credential);
-
-        // This next is for decoding the idToken to an object if you want to see the details.
-        let base64Url = response.credential.split('.')[1];
-        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        console.log(JSON.parse(jsonPayload));
-      }
+      callback: (response: any) => this.handleGoogleSignIn(response)
     });
     google.accounts.id.renderButton(
       document.getElementById("buttonDiv"),
       { size: "large", type: "icon", shape: "pill" }  // customization attributes
     );
+  }
+
+  handleGoogleSignIn(response: any) {
+    console.log(response.credential);
+
+    // This next is for decoding the idToken to an object if you want to see the details.
+    let base64Url = response.credential.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    console.log(JSON.parse(jsonPayload));
   }
 
 }
